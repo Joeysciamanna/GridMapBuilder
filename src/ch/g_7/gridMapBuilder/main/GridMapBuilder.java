d2ad27304b7aa6579214d6b238c03fcf6a8e4f9f
Joey Sciamanna <Joey Sciamanna@192.168.1.114>
2018-12-04 18:56:46 +0100

    .

--- src/ch/g_7/gridMapBuilder/main/GridMapBuilder.java
+++ src/ch/g_7/gridMapBuilder/main/GridMapBuilder.java
@@ -3,6 +3,8 @@ package ch.g_7.gridMapBuilder.main;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Point;
+import java.io.File;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import ch.g_7.gridEngine.core.FieldGrid;
@@ -10,6 +12,7 @@ import ch.g_7.gridEngine.field.building.DefaultFieldFactory;
 import ch.g_7.gridEngine.field.building.FieldCode;
 import ch.g_7.gridEngine.field.building.FieldFactory;
 import ch.g_7.gridEngine.field.building.FieldFactoryProducer;
+import ch.g_7.gridEngine.stream.MapReader;
 import ch.g_7.gridMapBuilder.builder.FieldCodeList;
 import ch.g_7.gridMapBuilder.builder.Placer;
 import ch.g_7.gridMapBuilder.builder.PlacersKeyListner;
@@ -27,14 +30,14 @@ public class GridMapBuilder {
 		fields.addField(new FieldCode("COLORED_FIELD",String.valueOf(Color.BLUE.getRGB())));
 		
 		
-//		String mapPath = "map";
-//		FieldGrid grid = new MapReader(new File(mapPath+".xml")).read().getGrid();
+		String mapPath = "map";
+		FieldGrid grid = new MapReader(new File(mapPath+".xml")).read().getGrid();
 
-		int fieldHeight = 16;
-		int fieldWidth = 16;
-		int height = 3;
-		int width = 3;
-		FieldGrid grid = new FieldGrid(new Dimension(fieldWidth,fieldHeight), new Dimension(width, height));
+//		int fieldHeight = 16;
+//		int fieldWidth = 16;
+//		int height = 12;
+//		int width = 12;
+//		FieldGrid grid = new FieldGrid(new Dimension(fieldWidth,fieldHeight), new Dimension(width, height));
 		
 		
 		Placer placer = new Placer(fields);
